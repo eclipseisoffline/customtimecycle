b216a3eaee6057a8aa90303c8db0cd6d491762f7
Eclipse <eclipse@eclipseisoffline.xyz>
2025-10-06 12:30:51 +0000

    Bump this

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
 multimod.fabric(project(":common"))
 
 dependencies {
-    // TODO fix this in multimod
     add("modImplementation", libs.fabric.permissions.api)
     add("include", libs.fabric.permissions.api)
 }
