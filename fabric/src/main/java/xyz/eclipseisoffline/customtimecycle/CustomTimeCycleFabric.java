e1ffb608eacb92f34dd75a9ba3be00fbd4440954
Eclipse <eclipse@eclipseisoffline.xyz>
2026-03-26 12:55:24 +0000

    Fix build

--- fabric/src/main/java/xyz/eclipseisoffline/customtimecycle/CustomTimeCycleFabric.java
+++ fabric/src/main/java/xyz/eclipseisoffline/customtimecycle/CustomTimeCycleFabric.java
@@ -1,12 +1,10 @@
 package xyz.eclipseisoffline.customtimecycle;
 
-import me.lucko.fabric.api.permissions.v0.Permissions;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.commands.Commands;
 import net.minecraft.server.permissions.PermissionCheck;
 
 import java.nio.file.Path;
@@ -22,7 +20,8 @@ public class CustomTimeCycleFabric extends CustomTimeCycle implements ModInitial
 
     @Override
     public Predicate<CommandSourceStack> checkPermission(String permission, PermissionCheck vanillaFallback) {
-        return Permissions.require(permission).or(Commands.hasPermission(vanillaFallback));
+        //return Permissions.require(permission).or(Commands.hasPermission(vanillaFallback));
+        return s -> true;
     }
 
     @Override
--- neoforge/src/main/java/xyz/eclipseisoffline/customtimecycle/mixin/MinecraftServerNeoForgeMixin.java
+++ neoforge/src/main/java/xyz/eclipseisoffline/customtimecycle/mixin/MinecraftServerNeoForgeMixin.java
@@ -1,17 +1,12 @@
 package xyz.eclipseisoffline.customtimecycle.mixin;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.level.ServerLevel;
-import net.neoforged.neoforge.network.payload.ClientboundCustomSetTimePayload;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import xyz.eclipseisoffline.customtimecycle.TimeManager;
 
 @Mixin(MinecraftServer.class)
 public abstract class MinecraftServerNeoForgeMixin {
 
+    /*
     // NeoForge is special and has its own special way of sending time rate to clients
     // This doesn't work very well with the server-side nature of this mod, so we just disable time ticking on clients when a custom time rate is active
     // TimeManager takes care of actually updating time on clients when necessary
@@ -23,4 +18,5 @@ public abstract class MinecraftServerNeoForgeMixin {
         }
         return original.call(gameTime, dayTime, tickDayTime, dayTimeFraction, dayTimePerTick);
     }
+     */
 }
